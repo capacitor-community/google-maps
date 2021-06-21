a553500c7ff10b58a9a6f89be444f4cb936935ce
tafelnl <35837839+tafelnl@users.noreply.github.com>
2021-06-21 16:09:34 +0200

    🔥 remove old types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,7 @@ import { registerPlugin } from "@capacitor/core";
 import { CapacitorGoogleMapsPlugin } from "./definitions";
 
 const CapacitorGoogleMaps = registerPlugin<CapacitorGoogleMapsPlugin>(
-    "CapacitorGoogleMaps",
-    {
-        web: () => import("./web").then((m) => new m.CapacitorGoogleMapsWeb()),
-    }
+  "CapacitorGoogleMaps"
 );
-
 export * from "./definitions";
 export { CapacitorGoogleMaps };
--- src/types/common/view.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface ViewID {
-    androidID?: number;
-    iOSID?: number;
-}
