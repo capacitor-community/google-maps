48c9ed8455a2213664b012e1ff76afb28227e1f5
tafelnl <35837839+tafelnl@users.noreply.github.com>
2021-08-22 14:18:05 +0200

    🔥 [ios] remove code in preparation of complete refactor

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -5,26 +5,4 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorGoogleMaps, "CapacitorGoogleMaps",
            CAP_PLUGIN_METHOD(initialize, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(create, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(addMarker, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(setMapType, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(setMapStyle, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(setCamera, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(isIndoorEnabled, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(accessibilityElementsHidden, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(isMyLocationEnabled, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(padding, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(clear, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(isTrafficEnabled, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(close, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(hide, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(show, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(reverseGeocodeCoordinate, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(settings, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(addPolyline, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(addPolygon, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(addCircle, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(enableCurrentLocation, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(myLocation, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(viewBounds, CAPPluginReturnPromise);
 )
