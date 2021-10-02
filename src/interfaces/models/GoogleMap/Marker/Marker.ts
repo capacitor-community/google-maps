9455900f5223ac8e192378e5fe46be442f3d2f6b
tafelnl <35837839+tafelnl@users.noreply.github.com>
2021-10-02 16:26:13 +0200

    🏷️ add `mapId` to `Marker` type

--- src/interfaces/models/GoogleMap/Marker/Marker.ts
+++ src/interfaces/models/GoogleMap/Marker/Marker.ts
@@ -1,5 +1,11 @@
 export interface Marker {
   /**
+   * GUID representing the map this marker is part of
+   *
+   * @since 2.0.0
+   */
+  mapId: string;
+  /**
    * GUID representing the unique id of this marker
    *
    * @since 2.0.0
