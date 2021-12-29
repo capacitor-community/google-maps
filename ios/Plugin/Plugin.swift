0191fafe34cf60854600ed9c2946d76f52b625dc
vorokami <vorokami@gmail.com>
2021-12-29 20:17:21 +0300

    finished "touch through html view to map" in iOS
    "cover map with HTMLElements" for iOS - solved
    I added overlayView and using it I calculate the touch
    if this touch point contains html element than
    view with html elements is on
    in another case view with html elements is off and
    touches pass through this view to map

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -16,30 +16,9 @@ public class CapacitorGoogleMaps: CustomMapViewEvents {
 
     var customMarkers = [String : CustomMarker]();
     
-    public var devicePixelRatio : Float = 0;
+    var hasTopView : Bool = false;
     
-    public var lastEventChainId : String = "";
-
-    public var previousEvents: [UIEvent] = []
-    public var delegateTouchEventsToMapId : String?;
-        
-    /**
-     This method should be called after we requested the WebView through notifyListeners("didRequestElementFromPoint").
-     It should tell us if the exact point that was being touched, is from an element in which a MapView exists.
-     Otherwise it is a 'normal' HTML element, and we should thus not delegate touch events.
-     */
-    @objc func elementFromPointResult(_ call: CAPPluginCall) {
-            var isSameNode : Bool? = call.getBool("isSameNode", false);
-            if (isSameNode != nil && isSameNode == true) {
-                // The WebView apparently has decide the touched point belongs to a certains MapView
-                // Now we should find out which one exactly.
-                var mapId : String? = call.getString("mapId");
-                if (mapId != nil) {
-                    self.delegateTouchEventsToMapId = mapId!;
-                }
-            }
-        call.resolve();
-    }
+    var arrayHTMLElements = [BoundingRect]();
 
 
     @objc func initialize(_ call: CAPPluginCall) {
@@ -52,7 +31,6 @@ public class CapacitorGoogleMaps: CustomMapViewEvents {
         }
 
         GMSServices.provideAPIKey(self.GOOGLE_MAPS_KEY)
-        self.devicePixelRatio = call.getFloat("devicePixelRatio", 0)
         call.resolve([
             "initialized": true
         ])
@@ -84,20 +62,18 @@ public class CapacitorGoogleMaps: CustomMapViewEvents {
             let twoDArray =  elementsOnTop["innerEles"] as? [[Int]];
             for i in 0..<twoDArray!.count {
                 var newBoundingRect = BoundingRect();
+                //     width: Math.round(boundingRectangleOfChild.width),
+                //     height: Math.round(boundingRectangleOfChild.height),
+                //     x: Math.round(boundingRectangleOfChild.x),
+                //     y: Math.round(boundingRectangleOfChild.y),
                 newBoundingRect.width = Double(twoDArray![i][0]);
                 newBoundingRect.height = Double(twoDArray![i][1]);
                 newBoundingRect.x = Double(twoDArray![i][2]);
                 newBoundingRect.y = Double(twoDArray![i][3]);
+                self.arrayHTMLElements.append(newBoundingRect);
             }
             
-            //     width: Math.round(boundingRectangleOfChild.width),
-            //     height: Math.round(boundingRectangleOfChild.height),
-            //     x: Math.round(boundingRectangleOfChild.x),
-            //     y: Math.round(boundingRectangleOfChild.y),
-            
-            
-            
-            
+
             self.bridge?.webView?.addSubview(customMapViewController.view)
 
             customMapViewController.GMapView.delegate = customMapViewController;
@@ -115,11 +91,6 @@ public class CapacitorGoogleMaps: CustomMapViewEvents {
             self.bridge?.webView?.isOpaque = false;
             self.bridge?.webView?.backgroundColor = UIColor.clear
 
-            // subview[1] - is html elements: buttons, textfields, etc
-            // it will always last element of webview.subviews, because we always
-            // send MapView to the back
-            var numberOfHTMLElementsOfView: Int = (self.bridge?.webView?.subviews.count)! - 1;
-            self.bridge?.webView?.subviews[numberOfHTMLElementsOfView].isUserInteractionEnabled = false
             
             // class for view that will receive touches and transmit them
             class OverlayView: UIView {
@@ -127,116 +98,60 @@ public class CapacitorGoogleMaps: CustomMapViewEvents {
                 public var mainClass: CapacitorGoogleMaps?;
                 
                 override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
-                        mainClass?.delegateTouchEventsToMapId = nil
-
-                        // Initialize JSObjects for resolve().
-                        var result : JSObject = JSObject()
-                        var coordinatesOfPoint : JSObject = JSObject()
-
-                        // Generate a UUID, and assign it to lastId.
-                        // This way we can make sure we are always referencing the last chain of events.
-                        mainClass!.lastEventChainId = NSUUID().uuidString.lowercased()
-
-                        // Then add it to result object, so the Webview can reference the correct events when needed.
-                        result.updateValue(mainClass!.lastEventChainId, forKey: "eventChainId")
-
-                        // Get the touched position.
-                        var x : Double = point.x
-                        var y : Double = point.y
-
-                        // Since pixels on a webpage are calculated differently, should convert them first.
-                        // Convert it to 'real' pixels in WebView by using devicePixelRatio
-//                        if (mainClass!.devicePixelRatio != nil && mainClass!.devicePixelRatio > 0) {
-//                            coordinatesOfPoint.updateValue(Float(x) / mainClass!.devicePixelRatio, forKey: "x")
-//                            coordinatesOfPoint.updateValue(Float(y) / mainClass!.devicePixelRatio, forKey: "y")
-//                        }
                     
-                    coordinatesOfPoint.updateValue(x, forKey: "x")
-                    coordinatesOfPoint.updateValue(y, forKey: "y")
-
-                        // Then add it to result object.
-                        result.updateValue(coordinatesOfPoint, forKey: "point");
-                        
-                        // Then notify the listener that we request to let the WebView determine
-                        // if the element touched is the same node as where some MapView is attached to.
+                    // if (count-1) is the top view, then hmtl layout is second and here must be (count-2)
+                    var countOfHTMLElementsOfView: Int = (mainClass?.bridge?.webView?.subviews.count)! - 2;
                     
-                    mainClass?.notifyListeners("didRequestElementFromPoint", data: result)
+                    // make html view default off
+                    mainClass?.bridge?.webView?.subviews[countOfHTMLElementsOfView].isUserInteractionEnabled = false
                     
-                    
-                    // if number -1 is the top view, then hmtl layaout is second and here must be -2
-                    var numberOfHTMLElementsOfView: Int = (mainClass?.bridge?.webView?.subviews.count)! - 2;
-
-                    mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].isUserInteractionEnabled = false
-                    
-                    if(mainClass?.delegateTouchEventsToMapId == nil)  {
-                        mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].isUserInteractionEnabled = true
-                    } else {
-                        mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].isUserInteractionEnabled = false
-                        mainClass?.customMapViewControllers[String(mainClass!.delegateTouchEventsToMapId!)]!.view.isUserInteractionEnabled = true;
+                    // checking the hit in the elements
+                    for boundingRect in mainClass?.arrayHTMLElements ?? [] {
+                        // checking the hit in the element
+                        if(point.x > boundingRect.x &&
+                           point.x < (boundingRect.x + boundingRect.width) &&
+                           point.y > boundingRect.y &&
+                           point.y < (boundingRect.y + boundingRect.height)) {
+                            // touch point inside of html element
+                            // then we make this subview is toucheable
+                            mainClass?.bridge?.webView?.subviews[countOfHTMLElementsOfView].isUserInteractionEnabled = true
+                            return false
+                        }
                     }
                     
+                    // if we here, than html view off
+                    let arrayOfMaps = [CustomMapViewController]((mainClass?.customMapViewControllers.values)!)
                     
+                    for mapview in arrayOfMaps {
+                        // checking if map exists in this point
+                        if(point.x > mapview.boundingRect.x &&
+                           point.x < (mapview.boundingRect.x + mapview.boundingRect.width) &&
+                           point.y > mapview.boundingRect.y &&
+                           point.y < (mapview.boundingRect.y + mapview.boundingRect.height)) {
+                        // if mapview exist in this point than doing nothing
+                        // just go further
+                            return false
+                        } else {
+                            // if there is no mapView than we on html
+//                            mainClass?.bridge?.webView?.subviews[countOfHTMLElementsOfView].isUserInteractionEnabled = true
+//                            return false
+                        }
+                                                
+                    }
+                    mainClass?.bridge?.webView?.subviews[countOfHTMLElementsOfView].isUserInteractionEnabled = true
                     return false
-                    
-                    
-//                    for var HTMLElement in mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].subviews ?? [] {
-//
-//
-//                    }
-                        
-                        
-                        
-//                    var numOfTouchedElem: Int = 0;
-//
-//                    mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].subviews
-////                        .dropFirst()
-//                        .forEach({ HTMLElement in
-//                            numOfTouchedElem+=1;
-//                        //skiping first element that take size of full webview
-//
-//                        // checking the hit in the element
-//                        if(point.x > HTMLElement.coordinateSpace.bounds.minX &&
-//                           point.x < HTMLElement.coordinateSpace.bounds.maxX &&
-//                           point.y > HTMLElement.coordinateSpace.bounds.minY &&
-//                           point.y < HTMLElement.coordinateSpace.bounds.maxY) {
-//                            // touch point inside of html element
-//                            // then we make this subview is toucheable
-//                            mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].isUserInteractionEnabled = true
-//                            print()
-//                            print("Number of touched View: " + String(numOfTouchedElem))
-//                            print()
-//                        } else {
-//                            // do nothing
-//                        }
-//                    })
-                    
-//                    for var HTMLElement: UIView in mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].subviews {
-//
-//
-//                        // checking the hit in the element
-//                        if(point.x > HTMLElement.coordinateSpace.bounds.minX &&
-//                           point.x < HTMLElement.coordinateSpace.bounds.maxX &&
-//                           point.y > HTMLElement.coordinateSpace.bounds.minY &&
-//                           point.y < HTMLElement.coordinateSpace.bounds.maxY) {
-//                            // touch point inside of html element
-//                            // then we make this subview is toucheable
-//                            mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].isUserInteractionEnabled = true
-//                        } else {
-//                            mainClass?.bridge?.webView?.subviews[numberOfHTMLElementsOfView].isUserInteractionEnabled = false
-//                        }
-//                    }
-                    
-                    
-                    
                 }
             }
             
-            var overlayView: OverlayView = OverlayView()
-            overlayView.mainClass = self
-            overlayView.isOpaque = true;
-            overlayView.backgroundColor = UIColor.clear
-            self.bridge?.webView?.addSubview(overlayView)
-            
+            if(self.hasTopView == false) {
+                let overlayView: OverlayView = OverlayView()
+                overlayView.mainClass = self
+                overlayView.isOpaque = true;
+                overlayView.backgroundColor = UIColor.clear
+                self.bridge?.webView?.addSubview(overlayView)
+                
+                self.hasTopView = true
+            }
             
         }
     }
