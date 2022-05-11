c51f141b99ab434bb342bbb368573bd6b6151a6a
tafelnl <35837839+tafelnl@users.noreply.github.com>
2022-05-11 11:48:19 +0200

    refactor a couple of things

--- android/src/main/java/com/hemangkumar/capacitorgooglemaps/IconDescriptor.java
+++ android/src/main/java/com/hemangkumar/capacitorgooglemaps/IconDescriptor.java
@@ -8,7 +8,9 @@ import androidx.annotation.NonNull;
 import com.getcapacitor.JSObject;
 
 class IconDescriptor {
+    @NonNull
     public final String url;
+    @NonNull
     public final Size size;
 
     private float density = Resources.getSystem().getDisplayMetrics().density;
@@ -26,18 +28,15 @@ class IconDescriptor {
      * @return IconDescriptor
      */
     public IconDescriptor(@NonNull final JSObject jsIcon) {
-        url = jsIcon.getString("url", "");
+        url = jsIcon.optString("url", "");
 
         final JSObject jsSize = JSObjectDefaults.getJSObjectSafe(
                 jsIcon,
                 "size",
                 new JSObject());
 
-        final String width = "width";
-        final String height = "height";
-
         size = new Size(
-                (int) Math.round(jsSize.optDouble(width, 30) * density),
-                (int) Math.round(jsSize.optDouble(height, 30) * density));
+                (int) Math.round(jsSize.optDouble("width", 30) * density),
+                (int) Math.round(jsSize.optDouble("height", 30) * density));
     }
 }
