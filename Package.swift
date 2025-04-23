a8e166f54c09ff76e224130d0311196e38b473c8
Vissie <19539422+Vissie2@users.noreply.github.com>
2025-04-23 14:12:13 +0200

    fix: remove errors

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
             targets: ["CapacitorGoogleMaps"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", branch: "6.0.0")
-        .package(url: "https://github.com/googlemaps/ios-maps-sdk.git", exact: "6.2.1")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "6.0.0"),
+        .package(url: "https://github.com/googlemaps/ios-maps-sdk.git", exact: "8.3.1"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", exact: "5.14.3")
     ],
     targets: [
@@ -19,8 +19,8 @@ let package = Package(
             name: "CapacitorGoogleMaps",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
-                .product(name: "GoogleMaps", package: "ios-maps-sdk")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "GoogleMaps", package: "ios-maps-sdk"),
                 .product(name: "SDWebImage", package: "SDWebImage")
             ],
             path: "ios/Plugin")
