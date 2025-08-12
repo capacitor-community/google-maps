0aba341621ddbca89b1d125d47909b87417d87ca
tafelnl <35837839+tafelnl@users.noreply.github.com>
2025-08-12 13:39:31 +0200

    style: consistent file naming with rest of repo

--- ios/Plugin/Utilities/UIView+isPixelTransparent.swift
+++ ios/Plugin/Utilities/UIView+isPixelTransparent.swift
@@ -1,15 +1,6 @@
-//
-//  UIView+isPixelTransparent+Extension
-//  Plugin
-//
-//  Created by Jarret on 12/08/2025.
-//  Copyright © 2025 Max Lynch. All rights reserved.
-//
-
 import UIKit
 
 extension UIView {
-
     func isPixelTransparent(at point: CGPoint) -> Bool {
         let scale = window?.screen.scale ?? contentScaleFactor
         let rect = CGRect(x: floor(point.x), y: floor(point.y), width: 1/scale, height: 1/scale)
