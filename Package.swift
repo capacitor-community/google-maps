6aac04d2f98d5ea44c8612fd71a2f0901c3b99ff
Jarret Hoving <54537997+JarretHoving97@users.noreply.github.com>
2026-04-01 15:33:10 +0200

    wip

--- Package.swift
+++ Package.swift
@@ -3,26 +3,37 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorCommunityGoogleMaps",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v14)],
     products: [
         .library(
             name: "CapacitorCommunityGoogleMaps",
-            targets: ["CapacitorGoogleMaps"])
+            targets: ["CapacitorCommunityGoogleMaps"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0")
+        .package(
+            url: "https://github.com/ionic-team/capacitor-swift-pm.git",
+            from: "7.0.0"
+        ),
+        .package(
+            url: "https://github.com/googlemaps/ios-maps-sdk.git",
+            from: "9.0.0"
+        ),
+        .package(
+            url: "https://github.com/SDWebImage/SDWebImage.git",
+            from: "5.0.0"
+        )
     ],
     targets: [
         .target(
-            name: "CapacitorGoogleMaps",
+            name: "CapacitorCommunityGoogleMaps",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "GoogleMaps", package: "ios-maps-sdk"),
+                .product(name: "SDWebImage", package: "SDWebImage")
             ],
-            path: "ios/Sources/CapacitorGoogleMaps"),
-        .testTarget(
-            name: "CapacitorGoogleMapsTests",
-            dependencies: ["CapacitorGoogleMaps"],
-            path: "ios/Tests/CapacitorGoogleMapsTests")
+            path: "ios/Sources/CapacitorGoogleMaps"
+        )
     ]
 )
